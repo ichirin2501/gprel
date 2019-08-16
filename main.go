080f1393786fc5283b93884d50d0816aafa0f4e9
ichirin2501 <ichirin.2501@gmail.com>
2019-08-17 00:30:58 +0900

    check sql/io thread

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +9,7 @@ import (
 	"github.com/ichirin2501/gprel/config"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 const (
@@ -17,19 +17,40 @@ const (
 	ExitCodeError
 )
 
-func isSlave(db *sql.DB) (bool, error) {
-	rows, err := db.Query("SHOW SLAVE STATUS")
+func isIOSQLRunning(db *sqlx.DB) (bool, error) {
+	rows, err := db.Queryx("SHOW SLAVE STATUS")
 	if err != nil {
 		return false, err
 	}
 	defer rows.Close()
-	if !rows.Next() { // Not a slave
-		return false, nil
+
+	var isSlave bool
+	for rows.Next() {
+		isSlave = true
+		d := make(map[string]interface{})
+		if err := rows.MapScan(d); err != nil {
+			return false, err
+		}
+
+		io := d["Slave_IO_Running"].([]byte)
+		sql := d["Slave_SQL_Running"].([]byte)
+
+		if string(io) != "Yes" {
+			return false, nil
+		}
+		if string(sql) != "Yes" {
+			return false, nil
+		}
 	}
+
+	if !isSlave {
+		return false, fmt.Errorf("not a slave")
+	}
+
 	return true, nil
 }
 
-func isRelayLogPurge(db *sql.DB) (bool, error) {
+func isRelayLogPurge(db *sqlx.DB) (bool, error) {
 	var v int
 	err := db.QueryRow("SELECT @@global.relay_log_purge AS Value").Scan(&v)
 	if err != nil {
@@ -38,14 +59,36 @@ func isRelayLogPurge(db *sql.DB) (bool, error) {
 	return v == 1, nil
 }
 
-func purgeRelayLogs(db *sql.DB) error {
-	if _, err := db.Exec("SET GLOBAL relay_log_purge = 1"); err != nil {
-		return err
-	}
+func purgeRelayLogs(db *sqlx.DB) error {
+	log.Println("Executing FLUSH NO_WRITE_TO_BINLOG RELAY LOGS")
 	if _, err := db.Exec("FLUSH NO_WRITE_TO_BINLOG RELAY LOGS"); err != nil {
 		return err
 	}
+
+	log.Println("Executing sleep delay...")
 	time.Sleep(time.Duration(config.Config.PurgeDelaySeconds) * time.Second)
+
+	// last check
+	log.Println("check SQL/IO Thread state")
+	if ok, err := isIOSQLRunning(db); !ok {
+		if err == nil {
+			return fmt.Errorf("stop slave?")
+		}
+		return err
+	}
+
+	log.Println("Executing SET GLOBAL relay_log_purge = 1")
+	if _, err := db.Exec("SET GLOBAL relay_log_purge = 1"); err != nil {
+		return err
+	}
+	log.Println("Executing FLUSH NO_WRITE_TO_BINLOG RELAY LOGS (again)")
+	if _, err := db.Exec("FLUSH NO_WRITE_TO_BINLOG RELAY LOGS"); err != nil {
+		return err
+	}
+
+	time.Sleep(3 * time.Second)
+
+	log.Println("Executing SET GLOBAL relay_log_purge = 0")
 	if _, err := db.Exec("SET GLOBAL relay_log_purge = 0"); err != nil {
 		return err
 	}
@@ -65,15 +108,15 @@ func run() (int, error) {
 	} else {
 		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?interpolateParams=true", config.Config.User, config.Config.Password, config.Config.Host, config.Config.Port, config.Config.DatabaseName)
 	}
-	db, err := sql.Open("mysql", dsn)
+	db, err := sqlx.Open("mysql", dsn)
 	if err != nil {
 		return ExitCodeError, err
 	}
 	defer db.Close()
 
-	if ok, err := isSlave(db); !ok {
+	if ok, err := isIOSQLRunning(db); !ok {
 		if err == nil {
-			return ExitCodeError, fmt.Errorf("target mysql server is not defined as replication slave")
+			return ExitCodeError, fmt.Errorf("SQL or IO Thread is not running")
 		}
 		return ExitCodeError, err
 	}
@@ -85,7 +128,6 @@ func run() (int, error) {
 		return ExitCodeError, err
 	}
 
-	log.Println("Executing SET GLOBAL relay_log_purge=1; FLUSH NO_WRITE_TO_BINLOG RELAY LOGS; sleeping..... SET GLOBAL relay_log_purge=0; ...")
 	if err := purgeRelayLogs(db); err != nil {
 		return ExitCodeError, err
 	}
@@ -100,7 +142,7 @@ func main() {
 
 	exitCode, err := run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s %v\n", time.Now().Format("2006/1/2 15:04:05"), err)
+		fmt.Fprintf(os.Stderr, "%s %v\n", time.Now().Format("2006/01/02 15:04:05"), err)
 	}
 	os.Exit(exitCode)
 }
